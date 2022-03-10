e6ebc3173c82e5e4dbfe9669177412ab7686ed77
Guido <guido@github.com>
2022-03-10 18:59:17 +0100

    Amended "-d" option

--- src/ob_usage.c
+++ src/ob_usage.c
@@ -13,11 +13,12 @@ void ob_version()
 void ob_usage()
 {
     fprintf(stderr,
-        "USAGE: " PACKAGE " [-l|-x|-h|-v|-V] image_file [file_arg]\n\n"
+        "USAGE: " PACKAGE " [-l|-x|-h|-v|-V] [-d dest_dir] image_file [file_arg]\n\n"
         "-l\tList directory of image_file\n"
         "\tIf file_arg is omitted: list all entries\n"
         "\totherwise, list files matching regex in file_arg\n\n"
         "-x\tExtract files on image_file\n\n"
+        "-d\tExtract into destination dir 'dest_dir' (must already exist)\n\n"
         "-v\tVerbose output\n\n"
        "-h\tShow this help information\n\n"
         "-V\tShow version information\n\n"
